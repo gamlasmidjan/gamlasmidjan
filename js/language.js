7f1998efa8d82c64a26426793db6fea4861b8395
Ægir Þorsteinsson <aegir@thorsteinsson.is>
2014-02-07 14:03:19 +0000

    Initial commit.

---
+++ js/language.js
@@ -0,0 +1,17 @@
+﻿
+$(function() {
+	$('#setEnglish').bind('click', function() {
+		$.cookie("lang", "en", { expires: 365 });
+		$('.is').fadeOut(function() { $('.en').fadeIn(); });
+	});
+	$('#setIcelandic').bind("click", function() {
+		$.cookie("lang", "is", { expires: 365 });
+		$('.en').fadeOut(function() { $('.is').fadeIn(); });
+	});
+
+	var currentCookie = $.cookie("lang");
+	if(currentCookie == "en") {
+		$('.is').hide();
+		$('.en').show();
+	}
+});
